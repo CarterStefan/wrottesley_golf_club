9f2bd6c6eb45ba5d63e487292dfd7a7cd55bfb15
CarterStefan <ssttefancarter@gmail.com>
2021-02-14 11:23:43 +0000

    Added search and select category functionality to homepage

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from . models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from . models import Product, Category
 
 # Create your views here.
 
@@ -8,9 +10,30 @@ def all_products(request):
     """ A view for the products page, including different categories, searching and sorting """
 
     products = Product.objects.all()
+    all_categories = Category.objects.all()
+    query = None
+    categories = None
+
+    if request.GET:
+        if 'filtered_category' in request.GET:
+            categories = request.GET['filtered_category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter something to search the store!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'all_categories': all_categories,
+        'current_categories': categories,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
