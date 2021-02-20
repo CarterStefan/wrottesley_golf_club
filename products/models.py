b7156299cdead5ca470675067d37f65a0dff41dc
CarterStefan <ssttefancarter@gmail.com>
2021-02-20 15:32:29 +0000

    Added size selector to models and products

--- products/models.py
+++ products/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     material = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
