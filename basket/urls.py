d7d2145826d70c160263a70ffdef1201c30868b3
CarterStefan <ssttefancarter@gmail.com>
2021-02-22 08:21:26 +0000

    Added functionality to update and remove item from basket

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<item_id>', views.add_to_basket, name='add_to_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('edit/<item_id>/', views.edit_basket, name='edit_basket'),
+    path('delete/<item_id>/', views.delete_from_basket, name='delete_from_basket'),
 ]
