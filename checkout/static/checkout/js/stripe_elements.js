debf6232ed809c3eb895547994b80fe7cd00e698
CarterStefan <ssttefancarter@gmail.com>
2021-03-07 11:47:01 +0000

    Added checkout form caching

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -49,38 +49,74 @@ var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({'disabled': true});
+    card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#submit-button-text').fadeToggle(100);
     $('#loading-spinner').fadeToggle(100);
-    // If the client secret was rendered server-side as a data-secret attribute
-    // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById("card-errors")
-            // Show error to your customer (e.g., insufficient funds)
-            console.log(result.error.message);
-            errorDiv.textContent = result.error.message;
-            $('#payment-form').fadeToggle(100);
-            $('#submit-button-text').fadeToggle(100);
-            $('#loading-spinner').fadeToggle(100);
-            card.update({'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            // The payment has been processed!
-            if (result.paymentIntent.status === 'succeeded') {
-            form.submit();
-            // Show a success message to your customer
-            // There's a risk of the customer closing the window before callback
-            // execution. Set up a webhook or plugin to listen for the
-            // payment_intent.succeeded event that handles any business critical
-            // post-payment actions.
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    }
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function () {
+        // If the client secret was rendered server-side as a data-secret attribute
+        // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    email: $.trim(form.email.value),
+                    phone: $.trim(form.phone_number.value),
+                    address:{
+                        line1: $.trim(form.street_address.value),
+                        city: $.trim(form.town_or_city.value),
+                        state: $.trim(form.county.value),
+                        country: $.trim(form.country.value)
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address:{
+                    line1: $.trim(form.street_address.value),
+                    city: $.trim(form.town_or_city.value),
+                    state: $.trim(form.county.value),
+                    postal_code: $.trim(form.postcode.value),
+                    country: $.trim(form.country.value)
+                }
             }
-        }
-    });
+        }).then(function(result) {
+            if (result.error) {
+                var errorDiv = document.getElementById("card-errors")
+                // Show error to your customer (e.g., insufficient funds)
+                errorDiv.textContent = result.error.message;
+                $('#payment-form').fadeToggle(100);
+                $('#submit-button-text').fadeToggle(100);
+                $('#loading-spinner').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                // The payment has been processed!
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                // Show a success message to your customer
+                // There's a risk of the customer closing the window before callback
+                // execution. Set up a webhook or plugin to listen for the
+                // payment_intent.succeeded event that handles any business critical
+                // post-payment actions.
+                }
+            }
+        });
+    }).fail(function () {
+        // relaod page, error will appear in django messages
+        location.reload();
+    })    
 });
