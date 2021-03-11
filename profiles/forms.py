9bd8ed39d7360c5a0343766c8d41d4e9040887ab
CarterStefan <ssttefancarter@gmail.com>
2021-03-11 18:34:54 +0000

    Added profile form

---
+++ profiles/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from .models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'phone number',
+            'default_street_address': 'street address',
+            'default_town_or_city': 'town or city',
+            'default_postcode': 'postcode',
+            'default_county': 'county or state',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'profile-form-input'
+            self.fields[field].label = False
