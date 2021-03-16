18635e8c819530bb73434476fa31da99b113027e
CarterStefan <ssttefancarter@gmail.com>
2021-03-16 20:20:14 +0000

    Added form widgets to edit blog page

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Blog, Comment
 
 
@@ -6,6 +7,8 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = '__all__'
+
+    main_image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
