68bed0c1a457d091eb697d4b97ed7f45ddbb9a15
CarterStefan <ssttefancarter@gmail.com>
2021-03-20 08:44:00 +0000

    Added new fields to blog post form

--- blog/forms.py
+++ blog/forms.py
@@ -6,14 +6,16 @@ from .models import Blog, Comment
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = ('slug','title', 'main_image', 'blog_content')
+        fields = ('slug', 'title', 'main_image',
+                  'sub_title_one', 'blog_content_one',
+                  'sub_title_two', 'blog_content_two')
 
-    main_image = forms.ImageField(label="Image", required=False, widget=CustomClearableFileInput)
+    main_image = forms.ImageField(label="Main Image", required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['blog_content'].widget.attrs['placeholder'] = 'Blog content'
+        self.fields['blog_content_one'].widget.attrs['placeholder'] = 'Blog content_one'
 
 
 class CommentForm(forms.ModelForm):
