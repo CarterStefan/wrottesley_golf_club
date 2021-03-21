913e5cf4ecdffbdec0ac5bb64f71b1532b8a0b7b
CarterStefan <ssttefancarter@gmail.com>
2021-03-21 17:22:23 +0000

    Added membership strip webhook view

--- memberships/urls.py
+++ memberships/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.memberships, name='memberships'),
+    path('success/', views.success, name='success'),
+    path('cancel/', views.cancel, name='cancel'),
     path('config/', views.stripe_config),
     path('create-checkout-session/', views.create_checkout_session),
+    path('webhook/', views.stripe_webhook),
 ]
