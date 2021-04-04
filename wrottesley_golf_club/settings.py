f8afcced76831e70f810a0c78fac24b2122178f3
CarterStefan <ssttefancarter@gmail.com>
2021-04-04 10:29:09 +0000

    Deployment step four - Added cache control

--- wrottesley_golf_club/settings.py
+++ wrottesley_golf_club/settings.py
@@ -181,6 +181,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'wrottesley-golf-club'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -197,7 +203,7 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
-    
+
 
 PRO_DELIVERY_CHARGE = 0
 AMATEUR_DELIVERY_CHARGE = 0
