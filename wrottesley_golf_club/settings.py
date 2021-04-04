6bfe296e94e1f0a112e2b9a5139d550a499a37cb
CarterStefan <ssttefancarter@gmail.com>
2021-04-04 09:14:49 +0000

    Removed secret key and set debug

--- wrottesley_golf_club/settings.py
+++ wrottesley_golf_club/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pj5(fk-0z!8zq5kvf*p^i-7aa1(0z=^x^!3n&474(*bvf1*qwv'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wrottesley-golf-club.herokuapp.com', 'localhost']
 
