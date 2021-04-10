4b8f0f6ffc2a220f608baf374ddf0f0b1bd0969b
CarterStefan <ssttefancarter@gmail.com>
2021-04-10 08:55:31 +0000

    Updated blog comments to be OneToOne field instead of charfield

--- blog/models.py
+++ blog/models.py
@@ -36,7 +36,7 @@ class Comment(models.Model):
         ordering = ['-date_created']
 
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
-    name = models.CharField(max_length=100)
+    name = models.OneToOneField(to=User, on_delete=models.CASCADE)
     body = models.CharField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
